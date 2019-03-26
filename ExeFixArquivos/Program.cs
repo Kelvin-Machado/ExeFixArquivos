6e1ebf60e3d344d1a5ba21a14858bfeaeeb004a6
Kelvin-Machado <kelvinmachado@live.com>
2019-03-26 15:27:26 -0300

    Modificando arquivo local

--- ExeFixArquivos/Program.cs
+++ ExeFixArquivos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
 
 namespace ExeFixArquivos
 {
@@ -6,7 +8,49 @@ namespace ExeFixArquivos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Fazer um programa para ler o caminho de um arquivo .csv contendo os dados de itens vendidos
+            //Cada item possui um nome, preço unitário e quantidade, separados por vírgula.
+            //Você deve gerar um novo arquivo chamado "summary.csv",
+            //localizado em uma subpasta chamada "out" a partir da pasta original do 
+            //arquivo de origem, contendo apenas o nome e o valor total para aquele
+            //item(preço unitário multiplicado pela quantidade).
+
+            string sourcePath = @"/Users/kelvin/Projects/arquivo/arquivo1.txt";
+            string targetPath = @"/Users/kelvin/Projects/arquivo/summary.csv";
+
+
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                string[] lines = File.ReadAllLines(sourcePath);
+
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string product = fields[0];
+                        double price = double.Parse((fields[1]), CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Summary summary = new Summary(product, price, quantity);
+
+                        sw.WriteLine(summary.Product + "," + summary.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
         }
     }
 }
---
+++ ExeFixArquivos/Summary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ExeFixArquivos
+{
+    public class Summary
+    {
+        public string Product { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Summary(string product, double price, int quantity)
+        {
+            Product = product;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price*Quantity;
+        }
+    }
+}
